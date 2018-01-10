f4bd2c76db29861fb6477d19626276e915abc2b7
Pavel <pavellarionov93@mail.ru>
2018-01-10 18:58:47 +0300

    Django app,sec commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'C:/Users/Павел/Desktop/qwerty.db'
+        'NAME': os.path.join(BASE_DIR, 'qwerty.db'),
     }
 }
 
